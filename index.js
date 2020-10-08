2ce4160033e53722c5c1c0c74d84d54566207c08
bowei <bowei.han100@gmail.com>
2020-10-07 22:41:26 -0400

    implementation

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ const S3 = new SDK.S3();
 const BUCKET = process.env.BUCKET;
 const BUCKET_URL = process.env.BUCKET_URL;
 
-exports.handler = function (event, context, callback) {
+const handler = function (event, context, callback) {
   console.log(event, context, callback);
-  const key = event.queryStringParameters.key;
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
+  const size = event.queryStringParameters.key;
+  const path = event.path.slice(1);
+  const key = `${size}/${path}`;
 
-  const width = parseInt(match[2], 10);
-  const height = parseInt(match[3], 10);
-  const originalKey = match[4];
+  const dimensions = size.split("x");
+  const width = parseInt(dimensions[0], 10);
+  const height = parseInt(dimensions[1], 10);
 
-  S3.getObject({ Bucket: BUCKET, Key: originalKey }, (data) =>
+  S3.getObject({ Bucket: BUCKET, Key: path }, (err, data) =>
     SHARP(data.Body)
       .resize(width, height)
       .toFormat("jpg")
@@ -24,17 +25,27 @@ exports.handler = function (event, context, callback) {
         S3.putObject(
           {
             Body: buffer,
-            Bucket,
+            Bucket: BUCKET,
             Key: key,
-            //   ContentType: "image/jpg",
+            ContentType: "image/jpeg",
+            ContentDisposition: "inline", // display images inline
           },
           () =>
             callback(null, {
               statusCode: "301",
               headers: { location: `${BUCKET_URL}/${key}` },
-              //   body: "",
+              body: "",
             })
         )
       )
   );
 };
+
+const event = {
+  queryStringParameters: {
+    key: "400x100",
+  },
+  path: "/theshot.jpg",
+};
+
+handler(event, null, () => {});
